c510a366555d4a0ea81072bae1e9eed8a16e58ee
Mrunal Nirajkumar Shah <mrunalnshah@protonmail.com>
2025-01-29 11:08:45 +0530

    Initial Design of Welcome Screen Done
    All Components are added as per design.
    
    https://github.com/mrunalnshah/Chronicles/issues/1

--- lib/screens/auth/welcome_screen.dart
+++ lib/screens/auth/welcome_screen.dart
@@ -1,66 +1,123 @@
 import 'package:flutter/material.dart';
 import 'package:chronicles/utilities/image_import/logo_import.dart';
-import 'package:chronicles/utilities/components/buttons.dart';
+import 'package:chronicles/utilities/components/buttons/infinite_width_button.dart';
 
 class WelcomeScreen extends StatelessWidget {
   // Logo Height and Width
   double logoWidth = 200.0;
   double logoHeight = 200.0;
 
-  // Button Size
-
-  // Button Text Style
-  final buttonLabelTextStyle = TextStyle(
-    color: Colors.white, // Text color
-    fontSize: 18,
-    fontWeight: FontWeight.bold,
+  // Chronicles Text Style
+  String chroniclesText = 'Chronicles';
+  final chroniclesTextStyle = TextStyle(
+    height: 0.9,
+    fontSize: 60.0,
+    fontFamily: 'Abyssinica_SIL',
   );
 
+  // Write Your Journey Text Style
+  String writeYourJourneyText = 'Write Your Journey';
+  double writeYourJourneyLeftPadding = 70.0;
+  final writeYourJourneyTextStyle = TextStyle(
+    fontSize: 20.0,
+    fontFamily: 'Hind',
+  );
+
+  // Button Label Text Style
+  double verticalButtonMargin = 7.0;
+  double googleButtonBorderWidth = 2.0;
+  double googleLogoTextPartitionWidth = 20.0;
+  String signInText = 'Sign In';
+  String signUpText = 'Sign Up';
+  String continueWithGoogleText = 'Continue With Google';
+  String logoPathForGoogle = 'assets/images/logo/googleLogo.png';
+
+  TextStyle buttonLabelTextStyle({required Color textColor}) {
+    return TextStyle(
+      color: textColor, // Dynamically set the text color
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  // Build Method Starts
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 2,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ImportLogo(width: logoWidth, height: logoHeight)
-                        .importLogowo(),
-                    Text('Chronicles'),
-                    Text('Write Your Journey'),
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  longWidthButton(
-                    onPress: () {},
-                    label: 'SignIn',
-                    textStyle: buttonLabelTextStyle,
+                  ImportLogo(width: logoWidth, height: logoHeight)
+                      .importLogowo(),
+                  Text(
+                    chroniclesText,
+                    style: chroniclesTextStyle,
                   ),
-                  longWidthButton(
-                    onPress: () {},
-                    label: 'SignUp',
-                    textStyle: buttonLabelTextStyle,
-                  ),
-                  longWidthButton(
-                    onPress: () {},
-                    label: 'Continue With Google',
-                    textStyle: buttonLabelTextStyle,
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: writeYourJourneyLeftPadding, top: 0.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        writeYourJourneyText,
+                        style: writeYourJourneyTextStyle,
+                      ),
+                    ),
                   ),
                 ],
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                infiniteRoundWidthButton(
+                  onPress: () {},
+                  buttonLabel: Text(
+                    signInText,
+                    style: buttonLabelTextStyle(textColor: Colors.white),
+                  ),
+                  verticalMargin: verticalButtonMargin,
+                ),
+                infiniteRoundWidthButton(
+                  onPress: () {},
+                  buttonLabel: Text(
+                    signUpText,
+                    style: buttonLabelTextStyle(textColor: Colors.white),
+                  ),
+                  verticalMargin: verticalButtonMargin,
+                ),
+                infiniteRoundWidthButton(
+                  onPress: () {},
+                  backgroundColor: Colors.white,
+                  borderWidth: googleButtonBorderWidth,
+                  borderColor: Colors.black,
+                  buttonLabel: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(logoPathForGoogle),
+                      SizedBox(
+                        width: googleLogoTextPartitionWidth,
+                      ),
+                      Text(
+                        continueWithGoogleText,
+                        style: buttonLabelTextStyle(textColor: Colors.black),
+                      ),
+                    ],
+                  ),
+                  verticalMargin: verticalButtonMargin,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/utilities/components/buttons/infinite_width_button.dart
+++ lib/utilities/components/buttons/infinite_width_button.dart
@@ -1,46 +1,52 @@
 import 'package:flutter/material.dart';
 
-class longWidthButton extends StatelessWidget {
+class infiniteRoundWidthButton extends StatelessWidget {
   double horizontalMargin;
   double verticalMargin;
   double horizontalPadding;
   double verticalPadding;
-  void Function()? onPress;
-  Color color;
+  double height;
+  double elevationValue;
+  Color backgroundColor;
+  Color borderColor;
   double circularBorderRadius;
-  String label;
-  TextStyle textStyle;
+  double borderWidth;
+  Widget buttonLabel;
+  void Function()? onPress;
 
-  longWidthButton({
+  infiniteRoundWidthButton({
+    this.height = 60.0,
     this.horizontalMargin = 20.0,
     this.verticalMargin = 0.0,
-    required this.onPress,
-    this.color = const Color(0xFF4EABCC),
-    this.circularBorderRadius = 25,
     this.verticalPadding = 15,
     this.horizontalPadding = 0,
-    required this.label,
-    required this.textStyle,
+    this.elevationValue = 10,
+    this.backgroundColor = const Color(0xFF4EABCC),
+    this.circularBorderRadius = 25,
+    this.borderWidth = 0,
+    this.borderColor = Colors.white,
+    required this.onPress,
+    required this.buttonLabel,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      height: height,
       margin: EdgeInsets.symmetric(
           horizontal: horizontalMargin, vertical: verticalMargin),
       child: MaterialButton(
         onPressed: onPress,
-        color: color,
+        color: backgroundColor,
+        elevation: elevationValue,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(circularBorderRadius),
+          side: BorderSide(color: borderColor, width: borderWidth),
         ),
         padding: EdgeInsets.symmetric(
             vertical: verticalPadding, horizontal: horizontalPadding),
-        child: Text(
-          label,
-          style: textStyle,
-        ),
+        child: buttonLabel,
       ),
     );
   }
